9f922c5c1e96197f60c8901c0c1b87d1e9b5ec8d
Gloomi <eee.mail.ph2@gmail.com>
2019-11-15 20:20:01 +0800

    test new packets

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -3,6 +3,9 @@
 
 #ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
 #define CONFIG_CUSTOM_DEFINES_PRE_HPP
+		#define PACKET_OBFUSCATION_KEY1 0x04F6B564
+		#define PACKET_OBFUSCATION_KEY2 0x4210EDD1
+		#define PACKET_OBFUSCATION_KEY3 0x1693F35E
 
 /**
  * rAthena configuration file (http://rathena.org)
