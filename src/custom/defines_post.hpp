5998dfcf198e5b99803cc1b7c7e5ca80b9960471
Gloomi <eee.mail.ph2@gmail.com>
2019-11-15 22:03:14 +0800

    Revert "packet obs keys"
    This reverts commit cf0337a85cffd54eec6091659f6547be575346b8.

--- src/custom/defines_post.hpp
+++ src/custom/defines_post.hpp
@@ -3,9 +3,7 @@
 
 #ifndef CONFIG_CUSTOM_DEFINES_POST_HPP
 #define CONFIG_CUSTOM_DEFINES_POST_HPP
-		#define PACKET_OBFUSCATION_KEY1 <0x5B485DA2>
-		#define PACKET_OBFUSCATION_KEY2 <0x0C386363>
-		#define PACKET_OBFUSCATION_KEY3 <0x7CE35280>
+
 /**
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
